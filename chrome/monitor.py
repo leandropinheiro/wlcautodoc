53992cb2f92e60930134e570e2c141a847344f80
leandropinheiro <leandro.pinheiro@pinetech.com.br>
2017-08-24 19:28:33 -0300

    Inicio do script para Chrome

--- chrome/monitor.py
+++ chrome/monitor.py
@@ -1,11 +1,14 @@
 import time
 from selenium import webdriver
 
+img = 10
+
 driver = webdriver.Chrome()  # Optional argument, if not specified will search path.
 driver.get('https://10.11.12.123')
 driver.maximize_window()
 assert driver.title == "Cisco Systems Login"
 driver.find_element_by_name("bSubmit ").click()
-driver.save_screenshot('screenie.png')
-
+driver.save_screenshot('monitor-'+str(img).zfill(2)+'.png')
+img += 1
+print (str(img).zfill(2))
 driver.quit()
