3de6317efe68e016aaca6769e8da9e2f82b4e28c
leandropinheiro <leandro.pinheiro@pinetech.com.br>
2017-08-30 10:52:43 -0300

    Fun! Fun! and more Fun! Monitor is complete, for now!

--- testes/monitor.py
+++ testes/monitor.py
@@ -9,7 +9,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 # Variaveis de acesso
 
 proto = "https"
-url = "10.11.12.123"
+url = "172.16.165.10"
 username = "admin"
 password = "Admin123"
 delay = 0
@@ -26,8 +26,10 @@ def screencap(Text, number):
     Text = gettext(Text)
     filename = modulo + '-' + str(number).zfill(3) + '-' + driver.title + '-' + Text + '.png'
     number += 1
+    getscrollsize()
     time.sleep(delay)
     driver.save_screenshot(filename)
+    driver.maximize_window()
 
     return number
 
@@ -41,7 +43,44 @@ def gettext(Text):
     
     return Text
 
-# Carrega o Webdriver chamando uma instancia do Chrome
+def getscrollsize():
+
+    driver.switch_to.default_content()
+
+    actual_size = driver.get_window_size()
+
+    actual_height = actual_size['height']
+    actual_width = actual_size['width']
+
+    navBar = driver.find_element_by_name('mainFrame')
+    driver.switch_to.frame(navBar)
+
+    navBar = driver.find_element_by_name('content')
+    driver.switch_to.frame(navBar)
+    
+    new_height = driver.execute_script("return Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);")
+    new_height += 63
+    new_width  = driver.execute_script("return Math.max(document.body.scrollWidth, document.body.offsetWidth, document.documentElement.clientWidth, document.documentElement.scrollWidth, document.documentElement.offsetWidth);")
+    new_width  += 180
+    
+    if new_height < actual_height: new_height = actual_height
+    if new_width < actual_width: new_width = actual_width
+
+    if new_height != actual_height or new_width != actual_width:
+        #driver.manage.window.resize_to(new_width, new_height)
+        driver.set_window_size(800, 600)
+        #driver.set_window_rect(x=-1000, y=-1000, width=new_width, height=new_height)
+        driver.set_window_position(-1000,-1000)
+        driver.set_window_size(new_width, new_height)
+    driver.switch_to.default_content()
+
+    navBar = driver.find_element_by_name('mainFrame')
+    driver.switch_to.frame(navBar)
+
+    navBar = driver.find_element_by_name('navBar')
+    driver.switch_to.frame(navBar)
+
+    return None
 
 driver = webdriver.Chrome()
 
@@ -53,7 +92,22 @@ driver.get(fullurl)
 # Maximiza a janela do Chrome
 # e garante que estamos na pagina correta
 
+actual_size = driver.get_window_size()
+
+print('height: ' + str(actual_size['height']))
+print('width: ' + str(actual_size['width']))
+
+#print(driver.get_window_size())
+# {'height': 1033, 'width': 945}
 driver.maximize_window()
+
+actual_size = driver.get_window_size()
+
+print('height: ' + str(actual_size['height']))
+print('width: ' + str(actual_size['width']))
+
+#print(driver.get_window_size())
+# {'height': 1053, 'width': 1920}
 assert driver.title == 'Cisco Systems Login'
 
 # Efetua o click no Botao Login
@@ -86,6 +140,7 @@ navBar2_id = 0
 # Entra e captura a primeira opcao do primeiro nivel do menu
 
 navBar1[navBar1_id].click()
+
 img = screencap(navBar1[navBar1_id].text, img)
 
 # Entra e captura da segunda opcao do primeiro nivel em diante
@@ -101,7 +156,6 @@ for menu in navBar2:
             navBar1[navBar1_id].click()
 
         menu.click()
-        #menu_name = gettext(html)
         img = screencap(html, img)
 
     navBar2_id += 1
